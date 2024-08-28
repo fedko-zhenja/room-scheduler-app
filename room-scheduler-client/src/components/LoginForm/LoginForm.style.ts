78679d66363eb051a175b00a8b78233e394e21e0
@fedko-zhenja <fedkozhenja@gmail.com>
2024-08-28 21:06:41 +0400

    feat: add link to registration page

--- room-scheduler-client/src/components/LoginForm/LoginForm.style.ts
+++ room-scheduler-client/src/components/LoginForm/LoginForm.style.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const LoginFormContent = styled.div`
-    padding: 50px;
+    padding: 50px 50px 10px;
     display: flex;
     flex-direction: column;
     align-items: center;
--- room-scheduler-client/src/components/pages/LoginPage.tsx
+++ room-scheduler-client/src/components/pages/LoginPage.tsx
@@ -2,12 +2,16 @@ import {FC} from "react";
 import { LoginForm } from "../LoginForm/LoginForm";
 import { Paths } from "../../types/type";
 import { LoginFormWrapper } from "../LoginForm/LoginForm.style";
+import { Link } from "react-router-dom";
 
 export const LoginPage: FC = () => {
     return (
         <LoginFormWrapper>
             <h3>Login</h3>
-            <LoginForm typeForm={Paths.LoginPage} />
+            <LoginForm typeForm={Paths.LoginPage}/>
+            <p>Not registered?
+                <Link to={Paths.RegistrationPage}> Click to register.</Link>
+            </p>
         </LoginFormWrapper>
     )
 }
